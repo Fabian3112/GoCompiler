38282d0600812461c8b4098ea16eeab1bbe78f00
Fabian Sand <fabian-sand@gmx.de>
2023-01-19 22:00:05 +0100

    Fix for short circuit evaluation

--- Evaluator.go
+++ Evaluator.go
@@ -112,25 +112,30 @@ func (e Plus) eval(s ValState) Val {
 
 func (e And) eval(s ValState) Val {
 	b1 := e[0].eval(s)
-	b2 := e[1].eval(s)
-	switch {
-	case b1.flag == ValueBool && !b1.valB:
+
+	if b1.flag == ValueBool && !b1.valB {
 		return mkBool(false)
-	case b1.flag == ValueBool && b2.flag == ValueBool:
+	}
+
+	b2 := e[1].eval(s)
+	if b1.flag == ValueBool && b2.flag == ValueBool {
 		return mkBool(b1.valB && b2.valB)
 	}
+
 	return mkUndefined()
 }
 
 func (e Or) eval(s ValState) Val {
 	b1 := e[0].eval(s)
-	b2 := e[1].eval(s)
-	switch {
-	case b1.flag == ValueBool && b1.valB:
+	if b1.flag == ValueBool && b1.valB {
 		return mkBool(true)
-	case b1.flag == ValueBool && b2.flag == ValueBool:
+	}
+
+	b2 := e[1].eval(s)
+	if b1.flag == ValueBool && b2.flag == ValueBool {
 		return mkBool(b1.valB || b2.valB)
 	}
+
 	return mkUndefined()
 }
 
